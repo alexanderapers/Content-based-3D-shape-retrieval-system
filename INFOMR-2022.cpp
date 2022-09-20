7e7cb3bbf72886022cc0780163dfb11beee11df7
Mathijs Bangma <m.bangma@students.uu.nl>
2022-09-20 16:31:32 +0200

    Include Eigen

--- INFOMR-2022.cpp
+++ INFOMR-2022.cpp
@@ -1,20 +1,14 @@
-// INFOMR-2022.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <Eigen/Dense>
+
+using Eigen::MatrixXd;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	MatrixXd m(2, 2);
+	m(0, 0) = 3;
+	m(1, 0) = 2.5;
+	m(0, 1) = -1;
+	m(1, 1) = m(1, 0) + m(0, 1);
+	std::cout << m << std::endl;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
