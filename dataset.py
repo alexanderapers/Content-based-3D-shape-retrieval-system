d28a4eebd29c93be4e7d33ef1115ef24c376f4dd
alexanderapers <alexander@apers.nl>
2022-09-28 16:22:51 +0200

    mesh and dataset classes

---
+++ dataset.py
@@ -0,0 +1,58 @@
+import csv
+import os
+from mesh import Mesh
+
+class Dataset:
+    def __init__(self,  folder_name_dataset, write_csv=False):
+        self.folder_name_dataset = folder_name_dataset
+        self.meshes_file_paths = self.get_all_meshes_file_paths()
+        self.meshes = self.get_all_meshes()
+        if write_csv:
+            self.write_csv()
+
+
+    def write_csv(self):
+        with open(os.getcwd() + "/basic_mesh_info.csv", "w") as conn:
+            writer = csv.writer(conn)
+            writer.writerow(["mesh_name", "category", "n_vertices", "n_faces"])
+            for mesh in self.meshes:
+                writer.writerow(mesh.basic_mesh_info())
+
+
+    def __iter__(self):
+        return self.meshes
+
+
+    def to_list(self):
+        return list(self.meshes)
+
+
+    def get_all_meshes(self):
+        """ Returns an iterator with all meshes """
+        for mesh_file_path in self.meshes_file_paths:
+            mesh = Mesh(mesh_file_path)
+            yield mesh
+
+
+    def get_all_meshes_file_paths(self):
+        """ Returns an iterator with all the file paths to meshes in the Princeton folder """
+        shape_dir = os.getcwd() + "/{}/".format(self.folder_name_dataset)
+        for folder in os.listdir(shape_dir):
+            if not folder.startswith("."):
+                for filename in os.listdir(shape_dir + folder):
+                    if filename.endswith(".ply"):
+                        file_path = shape_dir + folder + "/" + filename
+                        yield file_path
+
+
+    def get_mesh(self, mesh_name):
+        for mesh in self.meshes:
+            if mesh.name == mesh_name:
+                return mesh
+        raise Exception("This mesh was not found.")
+
+
+    def show_mesh(self, mesh_name):
+        for mesh_file_path in self.get_all_meshes_file_paths():
+            if mesh_file_path.split("/")[-1] == mesh_name:
+                Mesh(mesh_file_path).show()
--- main.py
+++ main.py
@@ -1,45 +1,10 @@
-import os
-import trimesh
 import sys
-
-def show_mesh(mesh_name):
-    for mesh_file_path in get_all_meshes():
-        if mesh_file_path.split("/")[-1] == mesh_name:
-            mesh = trimesh.load(mesh_file_path)
-            mesh.show()
-
-
-def get_all_meshes():
-    """ Returns an iterator with all the file paths to meshes in the Princeton folder """
-    shape_dir = os.getcwd() + "/Princeton/"
-    for folder in os.listdir(shape_dir):
-        if not folder.startswith("."):
-            for filename in os.listdir(shape_dir + folder):
-                if filename.endswith(".ply"):
-                    file_path = shape_dir + folder + "/" + filename
-                    yield file_path
-
-
-def get_info_meshes():
-    with open("basic_mesh_info.txt", "w") as conn:
-        conn.write("mesh_name class n_vertices n_faces\n")
-        for mesh_file_path in get_all_meshes():
-            name = mesh_file_path.split("/")[-1]
-            c = mesh_file_path.split("/")[-2]
-            mesh = trimesh.load(mesh_file_path)
-            n_vertices = mesh.vertices.shape[0]
-            n_faces = mesh.faces.shape[0]
-            if mesh.faces.shape[1] != 3:
-                print("We have some non-triangular meshes")
-
-            conn.write("{0} {1} {2} {3}\n".format(name, c, n_vertices, n_faces))
-
+from dataset import Dataset
 
 if __name__ == "__main__":
-    # to write basic mesh info to a txt file
-    #get_info_meshes()
+    dataset = Dataset("Princeton", write_csv = False)
 
     if len(sys.argv) == 2:
-        show_mesh(sys.argv[1])
+        dataset.show_mesh(sys.argv[1])
     else:
-        show_mesh("m0.ply")
+        dataset.show_mesh("m0.ply")
