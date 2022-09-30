812dcd7ef59593132369becc219e260cbac69396
enega001 <enega001@gold.ac.uk>
2022-09-30 12:00:50 +0200

    merge

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
-import trimesh
+import trimesh 
 import random
 
-if __name__ == "__main__":
-    # set paths to folder containing 3D shapes
+# if __name__ == "__main__":
+#     # set paths to folder containing 3D shapes
 
-    for filename in os.listdir(os.getcwd()):
-        if filename.endswith(".obj"):
-            # use trimesh to load and display 3D meshes
-            turtle_mesh = trimesh.load(filename)
-            turtle_mesh.show()
+#    #trimesh.util.attach_to_log()
+
+for filename in os.listdir(os.getcwd()):
+    if filename.endswith(".obj"):
+        # use trimesh to load and display 3D meshes
+        turtle_mesh = trimesh.load(filename, process = False)
+
+turtle_mesh.show(dict = {"wireframe": True})
