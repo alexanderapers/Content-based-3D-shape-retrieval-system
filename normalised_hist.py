9401573f31aab6f2d9c67b90ba0e505ea12b193c
enega001 <enega001@gold.ac.uk>
2022-10-12 17:11:54 +0200

    new stats

--- normalised_hist.py
+++ normalised_hist.py
@@ -30,42 +30,71 @@ df = pd.read_csv('csv/Princeton_normalized_basic_mesh_info.csv')
 # print(centroid_origin)
 
 #scaling distribution after normalization
-df3 = pd.read_csv('csv/Princeton_normalized_bounding_box.csv')
-A = np.array(df3.iloc[:, 1:])
-corners = []
-for i in range(8):
-    corner_xyz = A[:, 3*i : 3*i + 3]
-    corners.append(corner_xyz)
-    len(corners)
-    corners[-1]
-    np.linalg.norm([
-    [0,0,0],
-    [1,1,1]
-])**2
+# df3 = pd.read_csv('csv/Princeton_normalized_bounding_box.csv')
+# A = np.array(df3.iloc[:, 1:])
+# corners = []
+# for i in range(8):
+#     corner_xyz = A[:, 3*i : 3*i + 3]
+#     corners.append(corner_xyz)
+#     len(corners)
+#     corners[-1]
+#     np.linalg.norm([
+#     [0,0,0],
+#     [1,1,1]
+# ])**2
 
-euclideanDist = lambda u, v: np.linalg.norm([u, v])
+# euclideanDist = lambda u, v: np.linalg.norm([u, v])
 
-# distance between 2 corners for a given row
-def dist(row, i, j):  # i, j are corner-indices
-    # construct corner-vectors from indices
-    v_i = row[3*i : 3*i + 3]
-    v_j = row[3*j : 3*j + 3]
+# # distance between 2 corners for a given row
+# def dist(row, i, j):  # i, j are corner-indices
+#     # construct corner-vectors from indices
+#     v_i = row[3*i : 3*i + 3]
+#     v_j = row[3*j : 3*j + 3]
 
-    return euclideanDist(v_i, v_j)
+#     return euclideanDist(v_i, v_j)
 
-def maxDist(row):
-    dists = []
-    for i in range(8):
-        for j in range(i+1, 8):
-            dists.append(dist(row, i, j))
+# def maxDist(row):
+#     dists = []
+#     for i in range(8):
+#         for j in range(i+1, 8):
+#             dists.append(dist(row, i, j))
 
-    return max(dists)
+#     return max(dists)
 
-maxDist(A[0])
-maxDists = [maxDist(row) for row in A]
-plt.hist(maxDists, bins=64) ;
-plt.xlabel('corners')
-plt.ylabel('frequency')
-plt.title("Scaling distribution after normalization")
+# maxDist(A[0])
+# maxDists = [maxDist(row) for row in A]
+# plt.hist(maxDists, bins=64) ;
+# plt.xlabel('length of diagonal of bounding box')
+# plt.ylabel('frequency')
+# plt.title("Scaling distribution after normalization")
 
+# plt.show()
+
+#alignment after distribution 
+csv_name ="./csv/Princeton_normalized_alignment.csv"
+csv_file_name = csv_name.split("/")[-1]
+#print('File Name:', csv_name.split("\\")[-1])
+print(csv_name)
+df = pd.read_csv(csv_name)
+#plt_histogram(df,csv_file_name)
+df_raw =df
+#data_dict = df_raw.to_dict()
+plt_name = csv_file_name.split('.')[:1]
+plt_name = str(plt_name[0])
+
+fontsize =12
+df_raw.hist(column=['alignment_x'])
+plt.suptitle(plt_name, fontsize=fontsize)
+plt.ylabel("frequency")
+plt.xlabel(" x = [1,0,0]")
+
+df_raw.hist(column=['alignment_y'])
+plt.suptitle(plt_name, fontsize=fontsize)
+plt.ylabel("frequency")
+plt.xlabel("y = [0,1,0]")
+
+df_raw.hist(column=['alignment_z'])
+plt.suptitle(plt_name, fontsize=fontsize)
+plt.ylabel("frequency")
+plt.xlabel("z =  [0,0,1]")
 plt.show()
