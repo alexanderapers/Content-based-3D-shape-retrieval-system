4f8d9fcdaf8f053ac33182fcad15075258abecbb
Mathijs Bangma <m.bangma@students.uu.nl>
2022-10-19 15:11:24 +0200

    Add reorder script; include mesh folders in gitignore

---
+++ reorder.py
@@ -0,0 +1,73 @@
+import glob
+import os
+import numpy as np
+
+# HOW TO USE: copy to the Princeton(_remeshed(_normalized)) folder you want to reorder and just run the script!
+# DO NOT MOVE princeton_labels.txt!
+
+currentdir = os.getcwd()
+parentdir = os.path.dirname(currentdir)
+
+print("Starting file relabeling!")
+with open(parentdir + "/princeton_labels.txt") as f:
+    lines = f.readlines()
+
+    for line in lines:
+        args = line.split(' ')
+
+        label = args[0]
+        start = int(args[1])
+        end = int(args[2])
+
+        print("------------------------\nCATEGORY: " + label)
+        folder = currentdir + "/" + label
+        if os.path.exists(folder):
+            print("Folder " + folder + " already exists!")
+        else:
+            print("Making folder " + folder)
+            os.makedirs(folder)
+
+        numMeshes = end + 1 - start
+        moved = 0
+        skipped = 0
+
+        for i in range(start, end+1):
+            filename = "m" + str(i) + ".ply"
+            path = glob.glob(currentdir + "/**/" + filename)
+
+            # safeguard thing: make sure there's no deletions or duplicates
+            # tbh not quite sure what to do if there are any but at least you'll know
+            if len(path) == 0 or len(path) > 1:
+                if(len(path) == 0):
+                    print("Mesh " + filename + " not found!")
+                else:
+                    print("Mesh " + filename + " has " + len(path) + " copies!")
+                input()
+                quit()
+
+            oldpath = os.path.normpath(path[0])
+
+            newpath = os.path.normpath(folder + "/" + filename)
+            
+            if oldpath == newpath:
+                skipped += 1
+            else:
+                os.rename(oldpath, newpath)
+                moved += 1
+
+        print("Moved " + str(moved) + "/" + str(numMeshes) + " meshes. " + str(skipped) + " were in the correct category.")
+
+print("\n----------------\nMove completed. Attempting to remove empty folders...")
+emptydirs = list()
+for (dirpath, dirnames, filenames) in os.walk(currentdir):
+    if len(dirnames) == 0 and len(filenames) == 0:
+        emptydirs.append(dirpath)
+if len(emptydirs) > 0:
+    for dir in emptydirs:
+        print("Removing empty directory " + dir)
+        os.rmdir(dir)
+else:
+    print("No empty directories found. Why did you run this script if this directory is already sorted?")
+
+print("\n----------------\nDone! Press ENTER to exit.")
+input()
